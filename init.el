ed684127d25df5ca4f045deb828f23e8a39ceb8f
Todd Martin <kj4ntv@gmail.com>
2022-04-30 18:52:14 -0700

    Add go-mode

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 
 (setq package-list '(helm csharp-mode lsp-mode rustic neotree spacemacs-theme
 			  flycheck company omnisharp lsp-ui cargo dotnet magit
-			  restart-emacs doom-themes))
+			  restart-emacs doom-themes rust-mode go-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Getting MELPA
@@ -25,7 +25,7 @@
    [default default default italic underline success warning error])
  '(custom-enabled-themes '(doom-gruvbox))
  '(custom-safe-themes
-   '("c4063322b5011829f7fdd7509979b5823e8eea2abf1fe5572ec4b7af1dd78519" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "41098e2f8fa67dc51bbe89cce4fb7109f53a164e3a92356964c72f76d068587e" "ba72dfc6bb260a9d8609136b9166e04ad0292b9760a3e2431cf0cd0679f83c3a" "0cc49ad1ef7a63e9489f624a26878c2257f10607bd863635dfa196ecccfb9e2b" default))
+   '("a0be7a38e2de974d1598cf247f607d5c1841dbcef1ccd97cded8bea95a7c7639" "c4063322b5011829f7fdd7509979b5823e8eea2abf1fe5572ec4b7af1dd78519" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "41098e2f8fa67dc51bbe89cce4fb7109f53a164e3a92356964c72f76d068587e" "ba72dfc6bb260a9d8609136b9166e04ad0292b9760a3e2431cf0cd0679f83c3a" "0cc49ad1ef7a63e9489f624a26878c2257f10607bd863635dfa196ecccfb9e2b" default))
  '(exwm-floating-border-color "#252526")
  '(fci-rule-color "#37474F")
  '(highlight-tail-colors ((("#232a22" "#232a21") . 0) (("#283134" "#243034") . 20)))
@@ -52,7 +52,7 @@
  '(org-fontify-done-headline nil)
  '(org-fontify-todo-headline nil)
  '(package-selected-packages
-   '(spacemacs-theme neotree cargo dotnet omnisharp company lsp-ui flycheck lsp-mode magit rustic csharp-mode restart-emacs dashboard shanty-themes ewal-spacemacs-themes doom-themes helm-file-preview helm))
+   '(go-mode spacemacs-theme neotree cargo dotnet omnisharp company lsp-ui flycheck lsp-mode magit rustic csharp-mode restart-emacs dashboard shanty-themes ewal-spacemacs-themes doom-themes helm-file-preview helm))
  '(rustic-ansi-faces
    ["#1e1e1e" "#D16969" "#579C4C" "#D7BA7D" "#339CDB" "#C586C0" "#85DDFF" "#d4d4d4"])
  '(vc-annotate-background "#1e1e1e")
